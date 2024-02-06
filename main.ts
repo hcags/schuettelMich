7c5af7c96bbab276ab912c47bdf81dbd2394deed
hcags <120849679+hcags@users.noreply.github.com>
2024-02-06 05:32:49 -0800

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Gamer += -1
+    if (Gamer < 1) {
+        Gamer = 5
+    }
+    basic.showNumber(Gamer)
 })
+let Gamer = 0
+Gamer = 1
+basic.showNumber(Gamer)
