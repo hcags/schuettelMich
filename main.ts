f613d71a19fdc3b76a09ecbedc7c4cea264719b6
hcags <120849679+hcags@users.noreply.github.com>
2024-02-20 06:07:49 -0800

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,26 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    Gamer += -1
-    if (Gamer < 1) {
-        Gamer = 5
+    if (!(SaveGamer)) {
+        Gamer += -1
+        if (Gamer < 1) {
+            Gamer = 5
+        }
+        basic.showNumber(Gamer)
     }
-    basic.showNumber(Gamer)
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    SaveGamer = true
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    if (!(SaveGamer)) {
+        Gamer += 1
+        if (Gamer > 5) {
+            Gamer = 1
+        }
+        basic.showNumber(Gamer)
+    }
 })
 let Gamer = 0
+let SaveGamer = false
+SaveGamer = false
 Gamer = 1
 basic.showNumber(Gamer)
