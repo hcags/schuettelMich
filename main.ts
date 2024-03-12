8675f0c9d1581365d677e2db9bae5ddced25c9f4
hcags <120849679+hcags@users.noreply.github.com>
2024-03-12 14:54:58 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -28,7 +28,11 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     }
 })
 input.onGesture(Gesture.Shake, function () {
-    radio.sendNumber(Gamer)
+    if (Play) {
+        radio.sendNumber(Gamer)
+        Lifes += -1
+        basic.showString("" + (Lifes))
+    }
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "gameStart") {
