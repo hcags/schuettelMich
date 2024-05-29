a6bad064e3b5fdbc7d6b015dfc12f0963d88200c
hcags <120849679+hcags@users.noreply.github.com>
2024-05-29 01:07:25 -0700

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    if (!(SaveGamer)) {
+    if (!(SaveGamer) && !Play) {
         Gamer += -1
         if (Gamer < 1) {
             Gamer = 5
@@ -8,15 +8,17 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     }
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    if (!(SaveGamer)) {
+    if (!(SaveGamer) && !Play) {
         SaveGamer = true
         basic.showNumber(Lifes)
         basic.pause(500)
         basic.showNumber(Gamer)
+
+        basic.rgb(255,0,0)
     }
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    if (!(SaveGamer)) {
+    if (!(SaveGamer) && !Play) {
         Gamer += 1
         if (Gamer > 5) {
             Gamer = 1
@@ -25,6 +27,30 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     }
 })
 radio.onReceivedString(function (receivedString) {
+    let splittetString = receivedString.split('-')
+    if (splittetString[0] == 'difficult') {
+
+        basic.showString(splittetString[1])
+
+        Play = true
+        basic.rgb(0,255,255)
+        difficult = parseInt(splittetString[1])
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
     if (receivedString == "gameStart") {
         Play = true
         basic.showLeds(`
@@ -62,29 +88,10 @@ radio.onReceivedString(function (receivedString) {
             . . . . .
             # . . . #
             `)
+        basic.rgb(0,255,0)
         basic.showNumber(Lifes)
     }
 })
-radio.onReceivedNumber(function (receivedNumber) {
-    Play = true
-    difficult = receivedNumber
-    basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . . . .
-            . . # . .
-            `)
-    basic.pause(1000)
-    basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-})
-
 let difficult = 1
 let Lifes = 5
 let Gamer = 1
@@ -94,46 +101,49 @@ radio.setGroup(1)
 SaveGamer = false
 Play = false
 basic.showNumber(Gamer)
-
 input.onGesture(Gesture.Shake, function () {
     if (difficult >= 1) {
         shaked()
     }
 })
-
-
 input.onGesture(Gesture.LogoDown, function () {
     if (difficult >= 2) {
         shaked()
     }
 })
-
 input.onGesture(Gesture.ScreenUp, function () {
     if (difficult >= 2) {
         shaked()
     }
 })
-
 input.onGesture(Gesture.ScreenDown, function () {
     if (difficult >= 2) {
         shaked()
     }
 })
-
 input.onGesture(Gesture.ThreeG, function () {
     if (difficult > 2) {
         shaked()
     }
 })
-
 function shaked() {
     if (Play) {
         radio.sendNumber(Gamer)
         Lifes += -1
         if (Lifes > 0) {
             basic.showString("" + (Lifes))
+            if (Lifes < 3) {
+                basic.rgb(0,255,255)
+            }
         } else {
             basic.showIcon(IconNames.Skull)
+            basic.rgb(255, 0, 0)
         }
+        basic.pause(5000)
     }
 }
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function() {
+    radio.sendString('difficult-' + 2)
+    basic.pause(2000)
+    radio.sendString('gameStart')
+})
